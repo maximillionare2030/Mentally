48cef23ca76216a6eaec4ec8d526ca601eecf6ce
maximillionare2030 <maxtrinh4@gmail.com>
2024-12-15 00:36:40 -0800

    Initial commit!

---
+++ mentally-backend/api/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    api_url = os.getenv("API_URL", "default_value")
+    return {"message": f"API URL is {api_url}"}
